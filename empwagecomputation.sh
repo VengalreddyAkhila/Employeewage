28ed2e843d74b71aad4b257697b721c6c3098378
divakar reddy <akhila.it1801@gmail.com>
2021-05-18 10:08:14 +0530

    Added:empwagecomputation

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,14 +1,1 @@
-#!/bin/bash -x
-
-
-empCheck=$((RANDOM%2))
-ispresent=1
-EMP_RATE_PER_HR=20
-EMP_HRS=8
-
-if [ $empCheck -eq $ispresent ]
-then
-    wage=$(($EMP_RATE_PER_HR*$EMP_HRS))
-else 
-    wage=0
-fi
+Welcome to employeewagecomputation
